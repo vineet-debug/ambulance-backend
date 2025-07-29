6fdeaa1017d1c62fc7e714123106926e0053de6f
vineet-debug <vineet.dubey1985@gmail.com>
2025-07-29 12:33:55 +0530

    Update sendPush.js

--- utils/sendPush.js
+++ utils/sendPush.js
@@ -1,13 +1,22 @@
-const fetch = (...args) => import('node-fetch').then(({default: f}) => f(...args));
-const User  = require('../models/Driver'); // or union of both
+// backend/utils/sendPush.js
+const fetch = require('node-fetch');
 
-module.exports = async function sendPush(userId, message) {
-  const user = await User.findById(userId);
-  if (!user?.pushToken) return;
-
-  await fetch('https://exp.host/--/api/v2/push/send', {
-    method:'POST',
-    headers:{ 'Content-Type':'application/json' },
-    body: JSON.stringify([{ to: user.pushToken, ...message }]),
-  });
+module.exports = async function sendPush({ to, title, body, data }) {
+  try {
+    await fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        to,
+        title,
+        body,
+        sound: 'default',
+        data,
+      }),
+    });
+  } catch (err) {
+    console.error('Push notification error', err);
+  }
 };
