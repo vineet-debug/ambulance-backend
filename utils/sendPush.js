451d418d3608b8c7d19b4c5d2815babd613d8e28
vineet-debug <vineet.dubey1985@gmail.com>
2025-07-30 07:56:08 +0530

    Update sendPush.js

--- utils/sendPush.js
+++ utils/sendPush.js
@@ -1,20 +1,11 @@
-// backend/utils/sendPush.js
 const fetch = require('node-fetch');
 
 module.exports = async function sendPush({ to, title, body, data }) {
   try {
     await fetch('https://exp.host/--/api/v2/push/send', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        to,
-        title,
-        body,
-        sound: 'default',
-        data,
-      }),
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ to, title, body, sound: 'default', data }),
     });
   } catch (err) {
     console.error('Push notification error', err);
